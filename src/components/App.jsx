f5f1d5fb485329d1ffe46aeb3155824c1885bc7d
i.ilyev <i.ilyev@extra-ls.com>
2022-08-30 12:35:11 +0300

    todo React.lazy() и <Suspense>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,19 @@ import { Cast } from '../pages/MovieDetails/Cast/Cast';
 import { Reviews } from '../pages/MovieDetails/Reviews/Reviews';
 import { Search } from '../pages/Search/Search';
 
+export const URL_PREFIX = '/react-movie-search';
+
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Search />} />
-        <Route path="/movie/:movieId" element={<MovieDetails />}>
+        <Route path={URL_PREFIX + '/'} element={<Home />} />
+        <Route path={URL_PREFIX + '/movies'} element={<Search />} />
+        <Route path={URL_PREFIX + '/movie/:movieId'} element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Routes>
-
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import styles from './App.module.css'
+import { URL_PREFIX } from './App';
 
 export const Header = () => {
 
   return (
     <nav className={styles.header}>
-      <Link to="/">Home</Link>
-      <Link to="/movies">Movies</Link>
+      <Link to={URL_PREFIX + '/'}>Home</Link>
+      <Link to={URL_PREFIX + '/movies'}>Movies</Link>
     </nav>
   );
 };
